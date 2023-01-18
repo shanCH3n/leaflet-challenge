8b1209c3a6eed0c6d80c266d45b8cf5b34f7c792
shanCH3n <111955977+shanCH3n@users.noreply.github.com>
2023-01-18 13:58:00 +0800

    Legend TBC. Base map not displayed

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,67 +1,136 @@
-// Create function to display location of earthquakes
-function createMap(EQuakeLoc) {
-    
-    //// Create tile layer that wull be background of our map
-    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    
-    let baseMaps = {
-        "Street Map": streetmap
-    };
+// Leaflet-Part-1
 
-    let overlayMaps = {
-        "Earthquakes": EQuakeLoc
-    };
-    
-    // Create the map object with options. Centred on Australia
-    let myMap = L.map("map", {
-    center: [-25.274398, 133.775136],
-    zoom: 5,
-    layers: [streetmap, EQuakeLoc]
-    });
+// Store url to earthquake data in an object
+// Examine all Earthquakes in the past month
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer contol to the map
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(map);
-}
-
-
-// Load the GeoJSON data.
-let earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
-
-// Function to create markers
-
-function createMarkers(response) {
-
-    // Pull "features" property from response.features
-    let earthquakes = response.features;
-
-    // Initialize an array to hold coordinates of earthquakes
-    let quakeMarkers = [];
-
-    // Loop through the earthquakes array.
-    for (let index = 0; index < earthquakes.length; index++) {
-        let earthquake = earthquakes[index];
-        // For each earthquake location, create a circle marker, and bind a popup to be populated with its Magnitude, Depth and Location.
-        let quakeMarker = L.circleMarker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]])
-            .bindPopup(`<h3>Location: ${earthquake.properties.place}<br>Magnitude: ${earthquake.properties.mag}</h3>`);
-        // Add the marker to the quakeMarkers array.
-        quakeMarkers.push(quakeMarkers);
+// Perform a GET request to the URL
+d3.json(url).then(function (data) {
+    // Define a series of functions for each feature of the visualisation.
+    // Give each feature a popup displaying the place, magnitude, depth and time of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location: ${feature.properties.place}<br>
+        Magnitude: ${feature.properties.mag}<br>
+        Depth: ${feature.geometry.coordinates[2]}</h3>
+        <hr><p>${new Date(feature.properties.time)}</p>`);
     }
-    // Create a layer group that's made from the quakeMarkers array, and pass it to the createMap function.
-    createMap(L.layerGroup(quakeMarkers));
-}
 
-// Get the data with d3. Call createMarkers when it completes
-d3.json(earthquake_url).then(createMarkers);
+    // Create a circle marker for each earthquake
+    function createCircleMarker(feature, layer) {
+        let options = {
+            radius: MagRad(feature.properties.mag),
+            color: DepthColor(feature.geometry.coordinates[2]),
+            weight: 1,
+            opacity: 0.75,
+            fillOpacity: 0.40
+            
+        }
+        return L.circleMarker(layer, options);
+    }
+
+    // Radius of circle marker varies with magnitude - range [-1.0, 10.0]
+    function MagRad(magnitude) {
+        if (magnitude === 0) { //Scaling for Earthquakes with magnitude of 0 or less
+            return 1;
+        }
+        return magnitude * 5;
+    }
+
+    // Colour of circle marker varies with depth - range: [0,1000]
+    function DepthColor(depth) {
+        if (depth > 90) {
+            return "#330000";
+        }
+        if (depth > 80) {
+            return "#660000";
+        }
+        if (depth > 70) {
+            return "#990000";
+        }
+        if (depth > 60) {
+            return "#CC0000";
+        }
+        if (depth > 50) {
+            return "#CC6600";
+        }
+        if (depth > 40) {
+            return "#FF8000";
+        }
+        if (depth > 30) {
+            return "#FF9933";
+        }
+        if (depth > 20) {
+            return "#FFB266";
+        }
+        if (depth > 10) {
+            return "#FFFF00";
+        }
+        else {
+            return "#FFFF99";
+        }
+    }
 
 
+    // Create a GeoJSON layer containing the feature array on the earthquake data object.
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(data.features, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createCircleMarker
+    });
+
+    // Create base layers
+    var streetmap =  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    //var natgeo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+        //attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        //maxZoom: 16
+    //});
+
+    // Define baseMaps object to hold base layers
+    var baseMaps = {
+        "Street Map": streetmap,
+        //"Nat Geo View": natgeo
+    };
+
+    // Create overlayMaps object to hold overlay layers
+    var overLayMaps = {
+        "Earthquakes": earthquakes
+    };
+    
+    // Create map object
+    var myMap = L.map("map", { // reference to div id in html
+        centre: [-25.274398, 133.775136],
+        zoom: 3,
+        layers: [streetmap] // exclude natgeo for now
+    });
+
+    // Create layer control
+    L.control.layers(baseMaps, overLayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
+    
+    // Create Legend
+   //let legend = L.control({position: "bottomright"});
+    //legend.onAdd = function () {
+        //let div = L.DomUtil.create("div", "info legend");
+        //let colors = 
+        //let labels = [];
+
+    //}
+
+    // Add the legend to the map
+
+
+});
+
+
+// L.circleMarker
 // features.geometry.coordinates[0] = longitude
 // features.geometry.coordinates[1] = latitude
 // features.geometry.coordinates[2] = depth
-// features.properties.mag
-// features.properties.place
-// Activity 07 in Lession 15.1 for code on circles
+// features.properties.mag = Magnitude
+// features.properties.place = Place
 // Activity 04 in Lession 15.2 for Legend Code
